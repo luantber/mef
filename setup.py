969132737f7ef0c6c5095e5835b639d2332c4d45
Luis Bernal <luisbch9@gmail.com>
2022-04-01 00:11:18 -0300

    setup .py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mef',
+    version='0.0.1',
+    packages=['mef'],
+    # install_requires=[
+    #     'requests',
+    #     'importlib-metadata; python_version == "3.8"',
+    # ],
+)
